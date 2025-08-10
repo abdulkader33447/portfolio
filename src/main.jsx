7a0cf95d538442a6f3092ac8a65f9cf69594c390
abdulkader33447 <abdulkader33447@gmail.com>
2025-08-10 23:24:08 +0600

    added icon , bg color on root

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <div className="min-h-screen w-full bg-[#0f172a] relative text-white p-10">
+      {/* Blue Radial Glow Background */}
+      <div
+        className="absolute inset-0 z-0 pointer-events-none"
+        style={{
+          backgroundImage: `radial-gradient(circle 600px at 50% 50%, rgba(59,130,246,0.3), transparent)`,
+        }}
+      />
+      {/* Your Content/Components */}
+      <App />
+    </div>
+  </StrictMode>
+);
