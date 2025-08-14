618bb196bdedf73558fd2dc209721531e6ca4072
abdulkader33447 <abdulkader33447@gmail.com>
2025-08-14 10:21:44 +0600

    added logo click funtionaliti

--- src/Home/Pages/Footer/Footer.jsx
+++ src/Home/Pages/Footer/Footer.jsx
@@ -1,16 +1,45 @@
+import { useContext } from "react";
 import { FaFacebookSquare, FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import { LenisContext } from "../../../layouts/RootLayout";
 
 
 const Footer = () => {
     // console.log(logo);
+
+    const lenis = useContext(LenisContext);
+
+    const navbar_height = 140;
+
+    const handleScrollTo = (id) => {
+    if (id === "home") {
+      if (lenis) {
+        lenis.scrollTo(0, { duration: 2 });
+      } else {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      }
+      return;
+    }
+    const element = document.getElementById(id);
+    if (!element) return;
+
+    const offsetTop = element.offsetTop - navbar_height;
+
+    if (lenis) {
+      lenis.scrollTo(offsetTop, { duration: 2 });
+    } else {
+      element.scrollIntoView({ top: offsetTop, behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="sm:w-8/12 w-full mx-auto text-center lg:mt-40 md:mt-30 mt-20 lg:scroll-mt-40 md:scroll-mt-35 scroll-mt-25">
       <footer className="footer  sm:footer-horizontal text-neutral-content items-center p-4">
         <aside className="grid-flow-col items-center">
-          <img
+          <img 
+          onClick={()=>handleScrollTo("home")}
             src="https://i.ibb.co.com/1JJrHHpM/download.png"
             alt="icon"
-            className="size-10 border border-[#3b4c74] rounded-xl"
+            className="size-10 border border-[#3b4c74] rounded-xl cursor-pointer"
           />
           <p>
             Copyright © {new Date().getFullYear()} Abdul Kader - All right
