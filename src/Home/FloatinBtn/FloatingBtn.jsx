dc9d3c3c3981ce1b8c5f3d7ab35faff699937673
abdulkader33447 <abdulkader33447@gmail.com>
2025-09-28 12:52:32 +0600

    added duration whatsapp btn

--- src/Home/FloatinBtn/FloatingBtn.jsx
+++ src/Home/FloatinBtn/FloatingBtn.jsx
@@ -7,7 +7,7 @@ const FloatingBtn = () => {
         href="https://wa.me/8801568096650?text=Hello,%20I%20want%20to%20know%20about%20your%20services"
         target="_blank"
         rel="noopener noreferrer"
-        className="fixed bottom-5 right-5 bg-green-500 rounded-full p-3 shadow-lg hover:bg-green-600 transition"
+        className="fixed bottom-5 right-5 bg-green-500 rounded-full p-3 shadow-lg hover:bg-green-600 transition duration-500"
       >
         <IoLogoWhatsapp className="sm:text-4xl text-3xl"/>
       </a>
